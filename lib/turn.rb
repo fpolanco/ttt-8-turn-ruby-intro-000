9cf5e76536199d4255e03361fc7b0c15a0a14d84
Franchell Polanco <polanco.franchell@gmail.com>
2020-01-12 17:28:53 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -40,7 +40,7 @@ def turn(board)
   puts "Please enter 1-9:"
    input=gets.strip
    if valid_move?(board,input_to_index(input))
-        move(board,input)
+        move(board,input_to_index(input))
         display_board(board)
     else
         turn(board)
